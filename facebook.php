7be2e47d2ea8ca122fd53e45abdc0e919c8be036
quadriabdulsalam <99086729+quadriabdulsalam@users.noreply.github.com>
2022-07-02 16:05:15 -0500

    Create facebook.php

---
+++ facebook.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_POST['name']
+$email = $_POST['emil']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "dqutesalam@gmail.com";
+
+mail($recipient, $subject, $message ,$mailheader)
+or die("Error!");
+
+echo"message send";
+
+?>
