106adb1a0fdbb752be4f9dc242e202e4398c1739
quadriabdulsalam <99086729+quadriabdulsalam@users.noreply.github.com>
2022-07-02 16:52:07 -0500

    Update facebook.php

--- facebook.php
+++ facebook.php
@@ -1,15 +1,15 @@
 <?php
 
-$name = $_POST['name']
-$email = $_POST['emil']
-$subject = $_POST['subject']
-$message = $_POST['message']
+$name = $_POST['name'];
+$email = $_POST['emil'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
 $recipient = "dqutesalam@gmail.com";
 
-mail($recipient, $subject, $message ,$mailheader)
+mail($recipient, $subject, $message ,$mailheader);
 or die("Error!");
 
 header('Location:mail.html');
